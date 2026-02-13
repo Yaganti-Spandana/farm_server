301f9453de015ae0d532279d6b0e4cfc6ef9b90e
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-13 22:38:28 +0530

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -19,10 +19,11 @@ from django.urls import path,include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('bussiness.urls')),   
+    path('api/', include('bussiness.urls')),  
+    path("", lambda request: HttpResponse("Farm API is running")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
