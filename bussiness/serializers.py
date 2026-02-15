24a3324cc38751009bfde546b1fca11179a2bf33
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-15 14:43:05 +0530

    Update serializers.py

--- bussiness/serializers.py
+++ bussiness/serializers.py
@@ -11,6 +11,20 @@ class RegisterSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(**validated_data)
         return user
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        return data
+
 from .models import Animal,MilkRecord,Sale,Expense,FeedStock,FeedUsage
 
 class AnimalSerializer(serializers.ModelSerializer):
