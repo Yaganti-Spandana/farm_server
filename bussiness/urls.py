13fb606ffec92fa62671c78c68370851a19f9ce3
Yaganti.Spandana <spandanayaganti2@gmail.com>
2026-02-15 14:34:53 +0530

    Update urls.py

--- bussiness/urls.py
+++ bussiness/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path,include
-from .views import register
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+from .views import register, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import DefaultRouter
-from .views import AnimalViewSet,MilkRecordViewSet,SaleViewSet,ExpenseViewSet,FeedStockViewSet,FeedUsageViewSet,feed_remaining,profit_loss,monthly_report
+from .views import (
+    AnimalViewSet, MilkRecordViewSet, SaleViewSet,
+    ExpenseViewSet, FeedStockViewSet, FeedUsageViewSet,
+    feed_remaining, profit_loss, monthly_report
+)
 
 router = DefaultRouter()
 router.register('animals', AnimalViewSet, basename='animal')
@@ -12,13 +16,13 @@ router.register('expenses', ExpenseViewSet)
 router.register('feed-stock', FeedStockViewSet)
 router.register('feed-usage', FeedUsageViewSet)
 
-
 urlpatterns = [
     path('register/', register),
-    path('login/', TokenObtainPairView.as_view()),
+    path('login/', MyTokenObtainPairView.as_view()),  # 👈 custom
     path('refresh/', TokenRefreshView.as_view()),
     path('feed-remaining/', feed_remaining),
     path('profit-loss/', profit_loss),
     path('monthly-report/', monthly_report),
     path('', include(router.urls)),
 ]
+
