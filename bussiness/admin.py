3f9f7a71f7bd800b0d6ae2e479a0d3e0f36060f8
YagantiSpandana <yaganti06@gmail.com>
2026-02-17 15:19:57 +0530

    migrations

--- bussiness/admin.py
+++ bussiness/admin.py
@@ -1,29 +1,26 @@
 from django.contrib import admin
 from .models import Animal, MilkRecord, Sale, Expense, FeedStock, FeedUsage
 
-# Base class to make a model read-only in admin
+# Base class to make a model read-only
 class ReadOnlyAdmin(admin.ModelAdmin):
-    # make all fields read-only
-    readonly_fields = [f.name for f in admin.ModelAdmin.model._meta.get_fields()]
-    
-    # disable add, delete, and change permissions
+    # override get_readonly_fields per model
+    def get_readonly_fields(self, request, obj=None):
+        # get all field names for the model
+        return [field.name for field in self.model._meta.fields]
+
     def has_add_permission(self, request):
         return False
-    
+
     def has_change_permission(self, request, obj=None):
         return False
-    
+
     def has_delete_permission(self, request, obj=None):
         return False
 
-# Register models with the ReadOnlyAdmin
+# Register models with ReadOnlyAdmin
 admin.site.register(Animal, ReadOnlyAdmin)
 admin.site.register(MilkRecord, ReadOnlyAdmin)
 admin.site.register(Sale, ReadOnlyAdmin)
 admin.site.register(Expense, ReadOnlyAdmin)
 admin.site.register(FeedStock, ReadOnlyAdmin)
 admin.site.register(FeedUsage, ReadOnlyAdmin)
-
-
-
-
