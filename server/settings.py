2fea4a116ca512d47d257b7c20a48d587b1cafe9
YagantiSpandana <yaganti06@gmail.com>
2026-02-28 09:01:10 +0530

    modified

--- server/settings.py
+++ server/settings.py
@@ -81,7 +81,9 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://farm-dairy.netlify.app",
+]
 
 ROOT_URLCONF = 'server.urls'
 
