d8b02d913ea701bb7dc8a01f9214d55a02427bcd
YagantiSpandana <yaganti06@gmail.com>
2026-02-28 10:12:47 +0530

    modified

--- server/settings.py
+++ server/settings.py
@@ -49,13 +49,16 @@ INSTALLED_APPS = [
     'django_rest_passwordreset',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
